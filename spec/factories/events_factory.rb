40bcb0377fb09ac08353f9775ff334362abdfb17
Christopher Darrell <cdarrell@hypatia.local>
2018-07-22 19:23:06 -0700

    update factories for rails use

--- spec/factories/events_factory.rb
+++ spec/factories/events_factory.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :event, class: OpenStruct do
-    to_s "a cool event"
     to_create {}
   end
 end
--- spec/factories/forecast_factory.rb
+++ spec/factories/forecast_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :forecast, class: OpenStruct do
     rain? false
-    to_s "Cloudy with a chance of meatballs"
+    forecast "sunny"
     to_create {}
   end
 end
